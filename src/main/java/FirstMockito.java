e57f0908d05904647802a35c04437cfa6b2eb5d7
Bartek <bl4kee@wp.pl>
2021-03-17 18:06:53 +0100

    Initial commit

---
+++ src/main/java/FirstMockito.java
@@ -0,0 +1,2 @@
+public class FirstMockito {
+}
---
+++ src/main/java/com/bartekcodes/business/TodoBusinessImpl.java
@@ -0,0 +1,28 @@
+package com.bartekcodes.business;
+
+import com.bartekcodes.data.api.TodoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoBusinessImpl {
+
+    private TodoService todoService;
+
+    public TodoBusinessImpl(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    public List<String> retrieveTodosRelatedToSpring(String user) {
+        List<String> filteredTodos = new ArrayList<String>();
+        List<String> todos = todoService.retrieveTodos(user);
+
+        for(String todo: todos) {
+            if(todo.contains("Spring")) {
+                filteredTodos.add(todo);
+            }
+        }
+
+        return filteredTodos;
+    }
+}
